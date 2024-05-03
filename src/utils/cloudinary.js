9c77929c126279b6a5b85766445360a8f09f7b87
Ashish Tomer <135249489+ASHISHTOMER0817@users.noreply.github.com>
2024-05-04 00:58:02 +0530

    Update cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,7 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
           
-cloudinary.config({ 
-  cloud_name: 'dkjwc5n1v', 
-  api_key: '347523384144585', 
-  api_secret: 'KsmKHwAV0TgBEJAJtf3vBNfFQUA' 
-});
+
 
 
 const uploadCloudinary = async (localFilePath) =>{
