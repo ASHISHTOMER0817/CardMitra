afd66897aec2b04be598424a92150ce16bc71ce5
AdityaDentread <aditya@dentread.com>
2024-05-31 17:01:10 +0530

    mobile fixes - aditya

--- src/app/components/IntroHeader.tsx
+++ src/app/components/IntroHeader.tsx
@@ -3,39 +3,48 @@ import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/../public/logo.svg";
 const Header = () => {
-	const navbar = ["Home", "Features", "About Us", "Contact Us"];
+	// const navbar = ["Home", "About Us", "Features", "Testimonials", "Contact Us"];
+	const navbar = {
+		"Home": '',
+		"About Us": '#aboutUs',
+		"Features": '#ourFeatures',
+		"Testimonails": '#testimonials',
+		"Contact Us": '#contactUs',
+	}
 	return (
-		<div className="grid grid-rows-1 justify-between items-center grid-flow-col my-7 px-16 sm:gap-2 sm:justify-center ">
+		<div className="flex items-center mx-4 my-4 mb-1">
 			{" "}
 			<Link href={"/"} className="sm:w-8 sm:h-auto">
 				<Image src={Logo} alt={"Logo"} className=""></Image>
 			</Link>{" "}
-			<ul className="font-medium text-gray-400  flex gap-x-20 sm:text-nowrap sm:text-[10px] sm:gap-3">
-				{navbar.map((e, index) => {
-					return (
-						<li
-							key={index}
-							className="hover:text-black cursor-pointer focus:text-black hover:font-semibold transition-all"
-						>
-							{e}
-						</li>
-					);
-				})}
+			<ul className="font-medium text-gray-400 mx-auto header-items">
+				{
+					Object.entries(navbar).map(([key, value])=>{
+						return (
+							<li
+								key={key}
+								className="hover:text-black cursor-pointer focus:text-black hover:font-semibold transition-all"
+							>
+								<a href={value}>
+									{key}
+								</a>
+							</li>
+						);
+					})
+				}
 			</ul>
-			<div className="sm:text-nowrap sm:flex sm:text-[10px] sm:justify-center sm:items-center sm:gap-2 ">
-				<Link
-					className="px-6 py-3 mr-4 rounded-3xl font-semibold border-black border hover:bg-gray-100 hover:text-gray-600 sm:py-0 sm:mr-0 sm:px-[10px]"
-					href={"/Auth/login"}
-				>
-					Login
-				</Link>
-				<Link
-					className="bg-primaryBgClr text-white px-6 py-3 font-semibold rounded-3xl hover:text-gray-300 sm:px-[10px] sm:py-0"
-					href={"/Auth/signup"}
-				>
-					Sign Up
-				</Link>
-			</div>
+			<Link
+				className="button button-primary font-bold me-3 login-button"
+				href={"/Auth/login"}
+			>
+				Login
+			</Link>
+			<Link
+				className="button font-bold"
+				href={"/Auth/signup"}
+			>
+				Sign Up
+			</Link>
 		</div>
 	);
 };
