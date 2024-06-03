0ff23b3d2d46dd01ecd1cd142967f0068461fde8
AdityaDentread <aditya@dentread.com>
2024-06-04 03:12:48 +0530

    mobile fix - aditya

--- src/app/(authentication)/layout.tsx
+++ src/app/(authentication)/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				{/* <GlobalStateProvider>{children}</GlobalStateProvider> */}
 				{children}
 			</body>
 		</html>
--- src/app/components/InputSpace.tsx
+++ src/app/components/InputSpace.tsx
@@ -23,7 +23,7 @@ const InputSpace: React.FC<InputProps> = ({
 
 	return (
 		<input
-			className={`py-4 px-3 w-96 outline-none border border-gray-400 rounded-full sm:w-48 sm:py-2 sm:pl-6 ${classList}`}
+			className={`py-4 px-3 outline-none border border-gray-400 rounded-full sm:py-2 sm:pl-6 ${classList}`}
 			type={type}
 			placeholder={placeholder}
 			value={value}
