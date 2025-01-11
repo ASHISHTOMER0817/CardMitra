87136b93d4786c72c8c427018418adebd6cd6839
ASHISHTOMER0817 <ashish0817tomer@gmail.com>
2025-01-11 20:21:49 +0530

    bug fix - aditya

--- src/app/(user)/specialQuantity/page.tsx
+++ src/app/(user)/specialQuantity/page.tsx
@@ -36,65 +36,67 @@ const Deals = () => {
 		<div className="">
 			<h3 className="mb-4 mr-auto font-semibold">Special Requests</h3>
 			<div className="flex gap-6 flex-wrap justify-center">
+
+                {
+                    data? data.length? (
+                        <div className="overflow-x-auto bg-white shadow-md rounded-[8px] min-w-full">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-green-100">
+                                    <tr>
+                                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product</th>
+                                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Address</th>
+                                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
+                                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fullfilled</th>
+                                        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+                                    </tr>
+                                </thead>
+    
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                {
+                                    data?.map(
+                                        (
+                                            {
+                                                _id,
+                                                product,
+                                                user,
+                                                quantity, 
+                                                orderedQuantity
+                                            },
+                                            index
+                                        ) => {
+                                            return (
+                                                     
+                                                <tr key={'SP'+index} className={ index%2 ? 'bg-white' : 'bg-gray-50'}>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.name}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.address}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{quantity}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{orderedQuantity || 0}</td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                        {orderedQuantity != quantity && 
+                                                            <Link href={`/specialQuantity/${_id}`} className="text-primaryBgClr text-base sm:text-xs">
+                                                                Place Order
+                                                            </Link>
+                                                        }
+                                                    </td>
+                                                </tr>
+                                                        
+                                            );
+                                        }
+                                    )
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    )
+
+                    : (<Loader />) 
+
+                    : (<div className="text-red-500 font-serif mx-auto w-fit ">
+                            No special quantity to show...
+                        </div>)
+                }
 				
-				{!data.length ? (
-					<Loader />
-				) : data.length > 0 ? (
-                    <div className="overflow-x-auto bg-white shadow-md rounded-[8px] min-w-full">
-                        <table className="min-w-full divide-y divide-gray-200">
-                            <thead className="bg-green-100">
-                                <tr>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Address</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fullfilled</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
-                                </tr>
-                            </thead>
-
-                            <tbody className="bg-white divide-y divide-gray-200">
-                            {
-                                data?.map(
-                                    (
-                                        {
-                                            _id,
-                                            product,
-                                            user,
-                                            quantity, 
-                                            orderedQuantity
-                                        },
-                                        index
-                                    ) => {
-                                        return (
-                                                 
-                                            <tr key={'SP'+index} className={ index%2 ? 'bg-white' : 'bg-gray-50'}>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.name}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.address}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{quantity}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{orderedQuantity || 0}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                                    {orderedQuantity != quantity && 
-                                                        <Link href={`/specialQuantity/${_id}`} className="text-primaryBgClr text-base sm:text-xs">
-                                                            Place Order
-					                                    </Link>
-                                                    }
-                                                </td>
-                                            </tr>
-                                                    
-                                        );
-                                    }
-                                )
-                            }
-                            </tbody>
-                        </table>
-                    </div>
-
-					
-				) : (
-					<div className="text-red-500 font-serif mx-auto w-fit ">
-						No special quantity as such...
-					</div>
-				)}
+				
 			</div>
 		</div>
 	);
